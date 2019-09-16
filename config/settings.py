6dee9683e6d5e61181ce322a0f56fd4552fc002c
Alexey Lebedev <alexey.lebedev@fogstream.ru>
2019-09-16 21:57:37 +1000

    Add python-dotenv

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'REPLACE_ME')
