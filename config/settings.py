6d00de14ebdc7eedcc13822e67c35b61e5b92128
Timur Safin <timurtlt96@mail.ru>
2019-09-29 01:28:44 +0300

    Add model Profile

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4'
+    'bootstrap4',
+    'app'
 ]
 
 MIDDLEWARE = [
