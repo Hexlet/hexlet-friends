51e8484ed732a9deaf4ef27179d40c7c5b06dea9
ext_a.lebedev_ad@stash.bars-open.ru <lebedev.alexey@protonmail.com>
2019-10-02 10:40:16 +1000

    update setup.cfg, add docstring

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,5 @@
+"""Base tests module."""
+
 from http import HTTPStatus
 
 from django.conf import settings
@@ -5,11 +7,14 @@ from django.test import Client, TestCase
 
 
 class IndexPageTest(TestCase):
+    """Test index page view."""
 
     def setUp(self):
+        """Setup method."""
         self.client = Client()
 
     def test_index_page_view(self):
+        """Test index page 200 status."""
         response = self.client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIn(settings.APP_NAME, str(response.content))
--- app/views/index.py
+++ app/views/index.py
@@ -1,12 +1,15 @@
+"""Index page views."""
 from django.conf import settings
 from django.views.generic.base import TemplateView
 
 
 class IndexView(TemplateView):
+    """Index page view."""
 
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+        """Provide view page context."""
         context = super().get_context_data(**kwargs)
         context['APP_NAME'] = settings.APP_NAME
         return context
