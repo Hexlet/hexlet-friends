c8f6ce09ecd49953b2dba630874209399be07685
Anton <web-gambit@mail.ru>
2019-11-13 17:34:55 +0500

    Enrich list views with extra data and render it in tables

--- contributors/views/organizations.py
+++ contributors/views/organizations.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.views import generic
 
 from contributors.models.organization import Organization
@@ -6,6 +7,6 @@ from contributors.models.organization import Organization
 class ListView(generic.ListView):
     """A view for a list of organizations."""
 
-    model = Organization
+    queryset = Organization.objects.annotate(Count('repository'))
     template_name = 'organizations_list.html'
     context_object_name = 'organizations_list'
--- contributors/views/repositories.py
+++ contributors/views/repositories.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Sum
 from django.views import generic
 
 from contributors.models.repository import Repository
@@ -6,6 +7,10 @@ from contributors.models.repository import Repository
 class ListView(generic.ListView):
     """A view for a list of repositories."""
 
-    model = Repository
+    queryset = Repository.objects.select_related('organization').annotate(
+        pull_requests=Sum('contribution__pull_requests'),
+        issues=Sum('contribution__issues'),
+        contributors_count=Count('contribution'),
+    )
     template_name = 'repositories_list.html'
     context_object_name = 'repositories_list'
