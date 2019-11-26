703aa4fe94de8f29f2a8e0e3521167e8fc624210
Anton <web-gambit@mail.ru>
2019-11-26 10:51:48 +0500

    Update organization view and template

--- contributors/views/organization.py
+++ contributors/views/organization.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Sum
 from django.views import generic
 
 from contributors.models import Organization
@@ -8,3 +9,16 @@ class DetailView(generic.DetailView):
 
     model = Organization
     template_name = 'organization_details.html'
+
+    def get_context_data(self, **kwargs):
+        """Adds additional context for the organization."""
+        context = super().get_context_data(**kwargs)
+
+        repos_data = self.object.repository_set.annotate(
+            pull_requests=Sum('contribution__pull_requests'),
+            issues=Sum('contribution__issues'),
+            contributors_count=Count('contribution'),
+        )
+
+        context['repositories'] = repos_data
+        return context
