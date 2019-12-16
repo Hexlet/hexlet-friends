af10ef9ea2266c7ce4834b3705c59d16c084f186
Aleksei Pirogov <astynax@users.noreply.github.com>
2019-12-16 11:48:58 +0300

    update context manager

--- contributors/context_processors.py
+++ contributors/context_processors.py
@@ -6,4 +6,6 @@ def base_template_context(request: request) -> dict:  # noqa: WPS442
     """Build common application context."""
     return {
         'PROJECT_NAME': settings.PROJECT_NAME,
+        'GTM_ID': settings.GTM_ID,
+        'YANDEX_VERIFICATION': settings.YANDEX_VERIFICATION,
     }
