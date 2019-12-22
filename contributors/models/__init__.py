ad83cc8a1958d14d8623f537b53e8e75eeea53e1
Anton <web-gambit@mail.ru>
2019-12-22 21:13:34 +0500

    Add a model for commit stats

--- contributors/models/__init__.py
+++ contributors/models/__init__.py
@@ -1,4 +1,5 @@
 from contributors.models.base import CommonFields
+from contributors.models.commit_stats import CommitStats
 from contributors.models.contribution import Contribution
 from contributors.models.contributor import Contributor
 from contributors.models.organization import Organization
