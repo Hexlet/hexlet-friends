9275247ec73ed263be71784d2ed2c3998a3cf1e5
Anton <web-gambit@mail.ru>
2019-12-23 15:13:47 +0500

    Update Contribution representation in the admin

--- contributors/admin/contribution.py
+++ contributors/admin/contribution.py
@@ -1,24 +1,31 @@
 from django.contrib import admin
 
 from contributors.admin.custom import site
-from contributors.models import Contribution
+from contributors.models import CommitStats, Contribution
+
+
+class CommitStatsInline(admin.StackedInline):
+    """Commit statistics."""
+
+    model = CommitStats
+    extra = 0
 
 
 class ContributionAdmin(admin.ModelAdmin):
     """Contribution representation."""
 
+    inlines = (CommitStatsInline,)
     list_display = (
         'id',
         'repository',
         'contributor',
-        'commits',
-        'additions',
-        'deletions',
-        'pull_requests',
-        'issues',
-        'comments',
+        'type',
+        'html_url',
+        'created_at',
     )
-    list_filter = ('repository__organization', 'repository', 'contributor')
+    list_filter = (
+        'repository__organization', 'repository', 'contributor', 'type',
+    )
 
 
 site.register(Contribution, ContributionAdmin)
