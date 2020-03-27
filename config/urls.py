ac90c780735c68b6e0bf82c4d1480b3cad5add74
Anton <web-gambit@mail.ru>
2020-03-27 16:00:21 +0500

    Rearrange scripts

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
+from django.views.i18n import JavaScriptCatalog
 
 from contributors.admin.custom import site
 
 urlpatterns = [
     path('admin/', site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript_catalog'),
     path('auth/', include('django.contrib.auth.urls')),
     path('auth/', include('auth.urls')),
     path('', include('contributors.urls')),
---
+++ static/js/yearActivityChart.js
@@ -0,0 +1,56 @@
+function generateMonths() {
+const months = [];
+for (let i = 1; i <= 12; i += 1) {
+    months.push(moment().add(i,'months').format('MMM'));
+}
+return months;
+}
+
+const contributions = JSON.parse(document.getElementById('contributions').textContent);
+
+const ctx = document.getElementById('yearActivityChart').getContext('2d');
+const yearActivityChart = new Chart(ctx, {
+type: 'bar',
+data: {
+    labels: generateMonths(),
+    datasets: [
+    {
+        label: gettext('Commits'),
+        data: contributions.commits,
+        backgroundColor: 'rgba(87, 173, 219, 0.7)',
+    },
+    {
+        label: gettext('Pull requests'),
+        data: contributions.pull_requests,
+        backgroundColor: 'rgba(82, 206, 97, 0.7)',
+    },
+    {
+        label: gettext('Issues'),
+        data: contributions.issues,
+        backgroundColor: 'rgba(226, 113, 90, 0.7)',
+    },
+    {
+        label: gettext('Comments'),
+        data: contributions.comments,
+        backgroundColor: 'rgba(242, 232, 96, 0.7)',
+    },
+    ],
+},
+options: {
+    tooltips: {
+    mode: 'index',
+    intersect: false,
+    },
+    animation: {
+    duration: 0,
+    },
+    scales: {
+    xAxes: [{
+        stacked: true,
+    }],
+    yAxes: [{
+        stacked: true,
+    }],
+    },
+},
+});
