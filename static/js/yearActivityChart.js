b3bde5216f05692c1957b7ced26f098673cf0379
Anton <web-gambit@mail.ru>
2020-03-27 22:39:31 +0500

    Localize months' names on the chart

--- static/js/yearActivityChart.js
+++ static/js/yearActivityChart.js
@@ -1,56 +1,57 @@
 function generateMonths() {
-const months = [];
-for (let i = 1; i <= 12; i += 1) {
-    months.push(moment().add(i,'months').format('MMM'));
-}
-return months;
+  const months = [];
+  moment.locale(document.documentElement.lang);
+  for (let i = 1; i <= 12; i += 1) {
+    months.push(moment().add(i, 'months').format('MMM'));
+  }
+  return months;
 }
 
 const contributions = JSON.parse(document.getElementById('contributions').textContent);
 
 const ctx = document.getElementById('yearActivityChart').getContext('2d');
 const yearActivityChart = new Chart(ctx, {
-type: 'bar',
-data: {
+  type: 'bar',
+  data: {
     labels: generateMonths(),
     datasets: [
-    {
+      {
         label: gettext('Commits'),
         data: contributions.commits,
         backgroundColor: 'rgba(87, 173, 219, 0.7)',
-    },
-    {
+      },
+      {
         label: gettext('Pull requests'),
         data: contributions.pull_requests,
         backgroundColor: 'rgba(82, 206, 97, 0.7)',
-    },
-    {
+      },
+      {
         label: gettext('Issues'),
         data: contributions.issues,
         backgroundColor: 'rgba(226, 113, 90, 0.7)',
-    },
-    {
+      },
+      {
         label: gettext('Comments'),
         data: contributions.comments,
         backgroundColor: 'rgba(242, 232, 96, 0.7)',
-    },
+      },
     ],
-},
-options: {
+  },
+  options: {
     tooltips: {
-    mode: 'index',
-    intersect: false,
+      mode: 'index',
+      intersect: false,
     },
     animation: {
-    duration: 0,
+      duration: 0,
     },
     scales: {
-    xAxes: [{
+      xAxes: [{
         stacked: true,
-    }],
-    yAxes: [{
+      }],
+      yAxes: [{
         stacked: true,
-    }],
+      }],
     },
-},
+  },
 });
