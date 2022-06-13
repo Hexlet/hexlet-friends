8c3656c3e00fd1db7c6c20afae123fb7265235a1
emp7yhead <artyomkropp@gmail.com>
2022-06-13 22:54:37 +0900

    Remove switchLanguage function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,15 +2,3 @@
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-// Language selector
-function switchLanguage(e) {
-  e.preventDefault();
-  const langForm = document.querySelector('#language-form');
-  const selector = langForm.querySelector('select');
-  selector.value = e.target.innerText.trim().toLowerCase();
-  langForm.submit();
-}
-
-const langLinks = document.querySelectorAll('.inactive-lang-link');
-langLinks.forEach((link) => link.addEventListener('click', switchLanguage));
