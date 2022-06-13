b58a1aa221fbad6e616d0ca27338966a1dacf785
emp7yhead <artyomkropp@gmail.com>
2022-06-13 22:11:55 +0900

    Fix language code

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-en'
+LANGUAGE_CODE = 'en-gb'
 
 LANGUAGES = [
     ('en', 'English'),
