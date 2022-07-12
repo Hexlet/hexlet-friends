f4b10c1ab342178221edf5bb227962a1d56a36a8
emp7yhead <artyomkropp@gmail.com>
2022-07-13 00:48:22 +0900

    Add: pull requests list view

---
+++ contributors/views/pull_requests.py
@@ -0,0 +1,28 @@
+from django.views import generic
+
+from contributors.models.contribution import Contribution
+from contributors.views.mixins import TableSortSearchAndPaginationMixin
+
+
+class ListView(TableSortSearchAndPaginationMixin, generic.ListView):
+    """A view for a list of all pull requests."""
+
+    sortable_fields = (
+        'info__title',
+        'repository__full_name',
+        'contributor__login',
+        'html_url',
+        'created_at',
+        'info__is_open',
+    )
+    searchable_fields = (
+        'info__title',
+        'repository__full_name',
+        'contributor__login',
+        'created_at',
+        'html_url',
+    )
+    ordering = sortable_fields[0]
+
+    template_name = 'pull_requests_list.html'
+    queryset = Contribution.objects.filter(type='pr').all()
