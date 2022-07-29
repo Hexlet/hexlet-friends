3593d19071e2a62291f8d737602c73fe1a845c32
neihaoo <neihaoo@gmail.com>
2022-07-29 06:24:03 +0300

    fix navbar and tables styles

--- contributors/views/home.py
+++ contributors/views/home.py
@@ -14,7 +14,7 @@ class HomeView(TemplateView):
 
     template_name = 'home.html'
 
-    def get_context_data(self, **kwargs):   # noqa: WPS210
+    def get_context_data(self, **kwargs):  # noqa: WPS210
         """Add context."""
         context = super().get_context_data(**kwargs)
 
@@ -30,17 +30,19 @@ class HomeView(TemplateView):
         latest_issues = Contribution.objects.filter(
             repository__is_visible=True,
             type__in=['pr', 'iss'],
-        ).order_by('-created_at')[:10]
+        ).order_by('-created_at')[:11]
 
-        context.update({
-            'contributors_for_month': contributors_for_month,
-            'top10_committers': top10_committers,
-            'top10_requesters': top10_requesters,
-            'top10_reporters': top10_reporters,
-            'top10_commentators': top10_commentators,
-            'dt_month_ago': datetime_month_ago(),
-            'contributions_for_year': Contribution.objects.for_year(),
-            'latest_issues': latest_issues,
-        })
+        context.update(
+            {
+                'contributors_for_month': contributors_for_month,
+                'top10_committers': top10_committers,
+                'top10_requesters': top10_requesters,
+                'top10_reporters': top10_reporters,
+                'top10_commentators': top10_commentators,
+                'dt_month_ago': datetime_month_ago(),
+                'contributions_for_year': Contribution.objects.for_year(),
+                'latest_issues': latest_issues,
+            }
+        )
 
         return context
