2dbf79dab7cf5a3bf4a4ed331b6a26b0542c2b73
Dddarknight <9801677@gmail.com>
2022-11-18 21:19:00 +0300

    fix lint

--- contributors/views/projects.py
+++ contributors/views/projects.py
@@ -1,4 +1,4 @@
-from django.db.models import Count, Q
+from django.db.models import Count, Q  # noqa: WPS347
 from django.utils.translation import gettext_lazy as _
 from django.views import generic
 
@@ -10,7 +10,7 @@ class ListView(TableSortSearchAndPaginationMixin, generic.ListView):
     """A view for a list of projects."""
 
     queryset = Project.objects.prefetch_related(
-        'repository_set__contribution_set'
+        'repository_set__contribution_set',
     ).annotate(
         pull_requests=Count(
             'repository__contribution__id',
@@ -18,7 +18,7 @@ class ListView(TableSortSearchAndPaginationMixin, generic.ListView):
                 repository__contribution__type='pr',
                 repository__contribution__repository__is_visible=True,
                 repository__contribution__contributor__is_visible=True,
-                )
+            ),
         ),
         issues=Count(
             'repository__contribution__id',
@@ -26,7 +26,7 @@ class ListView(TableSortSearchAndPaginationMixin, generic.ListView):
                 repository__contribution__type='iss',
                 repository__contribution__repository__is_visible=True,
                 repository__contribution__contributor__is_visible=True,
-            )
+            ),
         ),
         commits=Count(
             'repository__contribution__id',
@@ -34,7 +34,7 @@ class ListView(TableSortSearchAndPaginationMixin, generic.ListView):
                 repository__contribution__type='cit',
                 repository__contribution__repository__is_visible=True,
                 repository__contribution__contributor__is_visible=True,
-            )
+            ),
         ),
         comments=Count(
             'repository__contribution__id',
@@ -42,12 +42,13 @@ class ListView(TableSortSearchAndPaginationMixin, generic.ListView):
                 repository__contribution__type='cnt',
                 repository__contribution__repository__is_visible=True,
                 repository__contribution__contributor__is_visible=True,
-            )
+            ),
         ),
         contributors_count=Count(
             'repository__contribution__contributor',
-            distinct=True),
-        )
+            distinct=True,
+        ),
+    )
 
     template_name = 'projects_list.html'
     context_object_name = 'projects_list'
