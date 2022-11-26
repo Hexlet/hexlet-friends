a5b3c320cacb3c74c99e74f96a3fff168a425ea7
Dddarknight <9801677@gmail.com>
2022-11-26 12:35:12 +0300

    change migration on Project

--- contributors/migrations/0010_alter_project_html_url.py
+++ contributors/migrations/0010_alter_project_html_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contributors', '0008_merge_20220714_0004'),
+        ('contributors', '0009_repository_owner_alter_repository_contributors_and_more in contributors'),
     ]
 
     operations = [
