d663aa5effc2c8955219c26d6e431ece521cff42
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-14 00:01:27 +0400

    Correct an error on the members page #210

--- contributors/views/contributors_for_period.py
+++ contributors/views/contributors_for_period.py
@@ -6,12 +6,18 @@ from contributors.views import contributors
 class ListView(contributors.ListView):
     """A list of contributors with monthly contributions."""
 
-    template_name = 'contributors_for_month.html'
+    template_name = 'contributors_for_period.html'
     context_object_name = 'contributors_list'
-    queryset = Contributor.objects.visible_with_monthly_stats()
 
     def get_context_data(self, **kwargs):
         """Add context."""
         context = super().get_context_data(**kwargs)
         context['dt_month_ago'] = misc.datetime_month_ago()
         return context
+
+    def get_queryset(self):
+        if self.extra_context.get('period') == 'week':
+            self.queryset = Contributor.objects.visible_with_weekly_stats()
+        elif self.extra_context.get('period') == 'month':
+            self.queryset = Contributor.objects.visible_with_monthly_stats()
+        return super().get_queryset()
