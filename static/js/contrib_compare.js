410c6e2a441a9b074e4c67fc32b9a29325387133
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-02-03 13:06:11 +0400

    update contrib_compare.js lint

--- static/js/contrib_compare.js
+++ static/js/contrib_compare.js
@@ -9,14 +9,16 @@ function MakeCompare(val){
     'cnt':'comments',
   }
 
-  if (val == -1) {
+  if (val === -1) {
     for (const key in fields) {
       $('#contributor_' + key).html('');
       $('#own-' + key).removeClass();
     }
-  }else {
-    var json_data = $.parseJSON($('#contributors_data').text());
-    var result = json_data.filter(el => {return el.id == val})[0];
+  } else {
+    const json_data = $.parseJSON($('#contributors_data').text());
+    const result = json_data.filter(el => {
+      return el.id == val
+    })[0];
     for (const key in fields) {
       $('#contributor_' + key).html(result[fields[key]]);
       ColoringField($('#own-'+key),result[fields[key]])
@@ -29,7 +31,7 @@ function  ColoringField(field, value){
   if (parseInt(field.text()) > value) {
     field.addClass('bg-success')
   } else {
-    if (parseInt(field.text()) == value) {
+    if (parseInt(field.text()) === value) {
       field.addClass('bg-warning')
     } else {
       field.addClass('bg-danger')
