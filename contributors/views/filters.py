a588dc1566c29b5bb718076a8cc83e5e75366d41
svetlana_akaemova <akaemova.sv@mail.ru>
2023-08-14 23:03:14 +0200

    Fix filters.py

--- contributors/views/filters.py
+++ contributors/views/filters.py
@@ -45,8 +45,8 @@ class IssuesFilter(django_filters.FilterSet):
     def get_good_first_issue(self, queryset, name, value):  # noqa: WPS110
         """Filter open issues by label 'good_first_issue'."""
         good_first = ContributionLabel.objects.get(
-            name='good_first_issue',
-        )
+            name='good first issue',
+        ).first()
         all_open_issues = Contribution.objects.filter(
             type='iss', info__state='open',
         )
