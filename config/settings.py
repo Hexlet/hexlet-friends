41aa0d3623fcb02aeacbf5df94b5218ffb7cf359
Nikolay Gagarinov <feycot@gmail.com>
2023-08-16 13:53:59 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -246,10 +246,10 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=0.01,
+    # traces_sample_rate=0.01,
     # To set a uniform sample rate
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production,
-    profiles_sample_rate=0.01,
+    # profiles_sample_rate=0.01,
 )
